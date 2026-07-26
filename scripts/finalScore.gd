0cc7ee4252b8ef5d69de6b74b1aa00b89d42fd5b
juliay <scjay07@gmail.com>
2026-07-26 11:30:52 -0300

    adicionei a formatação certa no score final

--- scripts/finalScore.gd
+++ scripts/finalScore.gd
@@ -1,10 +1,20 @@
 extends Label
 
+func formatar_tempo(segundos_totais: float) -> Array:
+	var minutos: int = int(segundos_totais) / 60
+	var segundos: int = int(segundos_totais) % 60
+	return [minutos, segundos]
 
+	return [minutos, segundos]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	text = "Your final score was: %.3f"%GameManager.score
-	pass # Replace with function body.
+	var tempo = formatar_tempo((GameManager.score/100))
+	var minutos: int = tempo[0]
+	var segundos: int = tempo[1]
+	
+	text = "Your final score was: %d minute(s) and %d second(s)" % [minutos, segundos]
+	
+	pass 
 
 
 func _on_play_again_button_pressed() -> void:
